4d153251cdfefcfc63b8d3625aae056fa2302ec8
Thommzy <tomiwaobeisun@yahoo.com>
2025-05-27 06:54:38 +0100

    Created Xcode Proj

---
+++ Ricktionary/Ricktionary/RicktionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RicktionaryApp.swift
+//  Ricktionary
+//
+//  Created by Timothy Obeisun on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct RicktionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
